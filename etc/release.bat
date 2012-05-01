fb615528916962bb869379d225ccb8430dcc0aaf
Yuzi <Yuzi@f4fb6e6c-418b-fd4c-a700-fdf38d0ce6e2>
2012-05-01 06:11:14 +0000

    【バッチ】 ・リリースバッチに、リリースフォルダへzipファイルをコピーする機能を追加
    git-svn-id: file:///E/Subversion/ExcelIncrementalSearch2@7 f4fb6e6c-418b-fd4c-a700-fdf38d0ce6e2

--- etc/release.bat
+++ etc/release.bat
@@ -1,4 +1,6 @@
 set FILE_NAME=excel_incremental_search2.zip
+set RELEASE_PATH=D:\Homepage\vector\excel_incremental_search
 del %FILE_NAME%
 cd ..\
 "C:\Program Files\Lhaplus\Lhaplus.exe" /c:zip /o:.\ /n:.\etc\%FILE_NAME% .\readme.html .\ExcelIncrementalSearch .\ExcelIncrementalSearch.xla .\register.vbs .\unregistere.vbs
+copy .\etc\%FILE_NAME% %RELEASE_PATH%
