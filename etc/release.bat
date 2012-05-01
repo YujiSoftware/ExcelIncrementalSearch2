690b4a75669042549940ee9a13854ae6a82623e9
Yuzi <Yuzi@f4fb6e6c-418b-fd4c-a700-fdf38d0ce6e2>
2012-05-01 10:12:33 +0000

    【バッチ】 ・リリースバッチに、サーバアップデート機能を追加
    git-svn-id: file:///E/Subversion/ExcelIncrementalSearch2@8 f4fb6e6c-418b-fd4c-a700-fdf38d0ce6e2

--- etc/release.bat
+++ etc/release.bat
@@ -1,6 +1,13 @@
 set FILE_NAME=excel_incremental_search2.zip
-set RELEASE_PATH=D:\Homepage\vector\excel_incremental_search
+set LOCAL_PATH=D:\Homepage\vector
+set RELEASE_PATH=%LOCAL_PATH%\excel_incremental_search
+set URI=ftp://VA049605@hp.vector.co.jp
+
+set LHAPLUS="C:\Program Files\Lhaplus\Lhaplus.exe"
+set WINSCP="D:\Software\WinSCP\WinSCP.exe"
+
 del %FILE_NAME%
 cd ..\
-"C:\Program Files\Lhaplus\Lhaplus.exe" /c:zip /o:.\ /n:.\etc\%FILE_NAME% .\readme.html .\ExcelIncrementalSearch .\ExcelIncrementalSearch.xla .\register.vbs .\unregistere.vbs
+%LHAPLUS% /c:zip /o:.\ /n:.\etc\%FILE_NAME% .\readme.html .\ExcelIncrementalSearch .\ExcelIncrementalSearch.xla .\register.vbs .\unregistere.vbs
 copy .\etc\%FILE_NAME% %RELEASE_PATH%
+%WINSCP% %URI% /synchronize %LOCAL_PATH% /
