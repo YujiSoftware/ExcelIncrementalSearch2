cfb799b3c23fa112ee93ac8d6abc8b35df065069
Yuzi <Yuzi@f4fb6e6c-418b-fd4c-a700-fdf38d0ce6e2>
2012-05-03 06:33:37 +0000

    【テストツール】 ・ExcelからHTMLを自動生成するツールを追加
    git-svn-id: file:///E/Subversion/ExcelIncrementalSearch2@12 f4fb6e6c-418b-fd4c-a700-fdf38d0ce6e2

---
+++ etc/test/CreateHtml.js
@@ -0,0 +1,23 @@
+try{
+    var files = ["search.xlsx", "search %23#.xlsx"];
+    
+    var objWshShell = new ActiveXObject("WScript.Shell");
+    var currentDir = objWshShell.CurrentDirectory;
+
+    var excel = WScript.CreateObject("Excel.Application");
+    excel.Visible = true;
+    excel.Workbooks.Open(currentDir + "\\..\\..\\" + "ExcelIncrementalSearch.xla");
+    
+    for(var i = 0, len = files.length; i < len; i++){
+        excel.Workbooks.Open(currentDir + "\\" + files[i]);
+        excel.Run("ExcelIncrementalSearch.xla!CreateExcelIncrementalSearch");
+    }
+
+}finally{
+    if(excel){
+      // Excel‚ð•Â‚¶‚é
+      excel.DisplayAlerts = false 
+      excel.Quit();
+      excel = null;
+    }
+}
