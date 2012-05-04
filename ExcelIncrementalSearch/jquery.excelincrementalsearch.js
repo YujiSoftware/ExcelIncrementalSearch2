2a74c08ecb86b7d0bcb385fb0e14237888d684a0
Yuzi <Yuzi@f4fb6e6c-418b-fd4c-a700-fdf38d0ce6e2>
2012-05-04 12:19:56 +0000

    【バージョンアップ】 ・ver2.0.2 に変更
    git-svn-id: file:///E/Subversion/ExcelIncrementalSearch2@17 f4fb6e6c-418b-fd4c-a700-fdf38d0ce6e2

--- ExcelIncrementalSearch/jquery.excelincrementalsearch.js
+++ ExcelIncrementalSearch/jquery.excelincrementalsearch.js
@@ -49,7 +49,7 @@ $(document).ready(function(){
         '          <a class="delete" title="検索キーワードを消去" onclick="$(\'#keyword\').val(\'\').trigger(\'keyup\').trigger(\'blur\').focus();"></a>',
         '        </div>',
         '      </form>',
-        '      <a class="product" href="http://hp.vector.co.jp/authors/VA049605/excel_incremental_search.html" target="_blank">Excelインクリメンタルサーチ ver2.0.1</a>',
+        '      <a class="product" href="http://hp.vector.co.jp/authors/VA049605/excel_incremental_search.html" target="_blank">Excelインクリメンタルサーチ ver2.0.2</a>',
         '  </div>',
         '</div>',
     ];
